d36094d3a3083a06dfa0b3a6492e3403d1ae51e0
Kevin Wentland <kwentland@threekit.com>
2022-03-07 17:31:15 -0600

    feat: use typescript

--- static/script.js
+++ static/script.js
@@ -1,106 +1,119 @@
-const getDataThen = (onSuccess) => {
-  $.ajax({
-    type: "GET",
-    url: `${window.location.origin}/static/movies.json`,
-    dataType: "json",
-    success: onSuccess,
-  });
-};
-
-const getUniqueActors = (movies) => {
-  const allActorsWithDuplicates = [];
-  movies.forEach((movie) => {
-    allActorsWithDuplicates.push(...movie.cast);
-  });
-  const uniqueActors = [...new Set(allActorsWithDuplicates)];
-  uniqueActors.sort();
-  return uniqueActors;
-};
-
-const getMovieGenres = (movies) => {
-  const allGenres = {};
-  movies.forEach((movie) => {
-    const myGenres = [...new Set(movie.genres)];
-    myGenres.forEach((genre) => {
-      if (allGenres[genre]) allGenres[genre]++;
-      else allGenres[genre] = 1;
-    });
-  });
-  return allGenres;
-};
-
-const homePageHandler = () => {
-  const $displayArea = $("#moviesInfo");
-  getDataThen((movies) => {
-    const genres = getMovieGenres(movies);
-    const actors = getUniqueActors(movies);
-    $displayArea.append(`<li>Number of Actors: ${actors.length}</li>`);
-    $displayArea.append(`<li>Number of Movies: ${movies.length}</li>`);
-    $displayArea.append(`<li>Number of Movies in Each Genre:</li>`);
-    Object.entries(genres).forEach(([genre, count]) => {
-      $displayArea.append(`<li>Number of ${genre} Movies: ${count}</li>`);
-    });
-  });
-};
-
-const actorsPageHandler = () => {
-  const $displayArea = $("#actorsList");
-  getDataThen((movies) => {
-    const uniqueActors = getUniqueActors(movies);
-    uniqueActors.forEach((actor) => {
-      $displayArea.append(
-        `<li><a href="${window.location.origin}/actor/${actor}">${actor}</a></li>`
-      );
-    });
-  });
-};
-
-const moviesPageHandler = () => {
-  const $displayArea = $("#moviesList");
-  getDataThen((movies) => {
-    const movieTitles = movies.map((movie) => movie.title).sort();
-    movieTitles.forEach((title) => {
-      $displayArea.append(
-        `<li><a href="${window.location.origin}/movie/${title}">${title}</a></li>`
-      );
-    });
-  });
-};
-
-const actorPageHandler = (actor) => {
-  const $displayArea = $("#actorInfo");
-  getDataThen((movies) => {
-    console.log(actor);
-    const myMovies = movies.filter((movie) => movie.cast.includes(actor));
-    myMovies.forEach((movie) => {
-      const title = movie.title;
-      $displayArea.append(
-        `<li><a href="${window.location.origin}/movie/${title}">${title}</a></li>`
-      );
-    });
-  });
-};
-
-const moviePageHandler = (movieTitle) => {
-  const $displayArea = $("#movieInfo");
-  getDataThen((movies) => {
-    const movie = movies.find((movie) => movie.title === movieTitle);
-    const actors = [...new Set(movie.cast)];
-    const genres = [...new Set(movie.genres)];
-    $displayArea.append(`<li>Year Released: ${movie.year}</li>`);
-    if (genres.length > 0) {
-      $displayArea.append(`<li>Movie Genres:</li>`);
-      genres.forEach((genre) => {
-        $displayArea.append(`<li>${genre}</li>`);
-      });
+var __read = (this && this.__read) || function (o, n) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator];
+    if (!m) return o;
+    var i = m.call(o), r, ar = [], e;
+    try {
+        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
     }
-    if (actors.length > 0) {
-      $displayArea.append(`<li>Movie Cast (${actors.length} actors):</li>`);
-      actors.forEach((actor) => {
-        $displayArea.append(
-          `<li><a href="${window.location.origin}/actor/${actor}">${actor}</a></li>`
-        );
-      });
+    catch (error) { e = { error: error }; }
+    finally {
+        try {
+            if (r && !r.done && (m = i["return"])) m.call(i);
+        }
+        finally { if (e) throw e.error; }
     }
-  });
+    return ar;
+};
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+var getDataThen = function (onSuccess) {
+    $.ajax({
+        type: "GET",
+        url: "".concat(window.location.origin, "/static/movies.json"),
+        dataType: "json",
+        success: onSuccess,
+    });
+};
+var getUniqueActors = function (movies) {
+    var allActorsWithDuplicates = [];
+    movies.forEach(function (movie) {
+        allActorsWithDuplicates.push.apply(allActorsWithDuplicates, __spreadArray([], __read(movie.cast), false));
+    });
+    var uniqueActors = __spreadArray([], __read(new Set(allActorsWithDuplicates)), false);
+    uniqueActors.sort();
+    return uniqueActors;
+};
+var getMovieGenres = function (movies) {
+    var allGenres = {};
+    movies.forEach(function (movie) {
+        var myGenres = __spreadArray([], __read(new Set(movie.genres)), false);
+        myGenres.forEach(function (genre) {
+            if (allGenres[genre])
+                allGenres[genre]++;
+            else
+                allGenres[genre] = 1;
+        });
+    });
+    return allGenres;
+};
+var homePageHandler = function () {
+    var $displayArea = $("#moviesInfo");
+    getDataThen(function (movies) {
+        var genres = getMovieGenres(movies);
+        var actors = getUniqueActors(movies);
+        $displayArea.append("<li>Number of Actors: ".concat(actors.length, "</li>"));
+        $displayArea.append("<li>Number of Movies: ".concat(movies.length, "</li>"));
+        $displayArea.append("<li>Number of Movies in Each Genre:</li>");
+        Object.entries(genres).forEach(function (_a) {
+            var _b = __read(_a, 2), genre = _b[0], count = _b[1];
+            $displayArea.append("<li>Number of ".concat(genre, " Movies: ").concat(count, "</li>"));
+        });
+    });
+};
+var actorsPageHandler = function () {
+    var $displayArea = $("#actorsList");
+    getDataThen(function (movies) {
+        var uniqueActors = getUniqueActors(movies);
+        uniqueActors.forEach(function (actor) {
+            $displayArea.append("<li><a href=\"".concat(window.location.origin, "/actor/").concat(actor, "\">").concat(actor, "</a></li>"));
+        });
+    });
+};
+var moviesPageHandler = function () {
+    var $displayArea = $("#moviesList");
+    getDataThen(function (movies) {
+        var movieTitles = movies.map(function (movie) { return movie.title; }).sort();
+        movieTitles.forEach(function (title) {
+            $displayArea.append("<li><a href=\"".concat(window.location.origin, "/movie/").concat(title, "\">").concat(title, "</a></li>"));
+        });
+    });
+};
+var actorPageHandler = function (actor) {
+    var $displayArea = $("#actorInfo");
+    getDataThen(function (movies) {
+        console.log(actor);
+        var myMovies = movies.filter(function (movie) { return movie.cast.includes(actor); });
+        __spreadArray([], __read(new Set(myMovies)), false).forEach(function (movie) {
+            var title = movie.title;
+            $displayArea.append("<li><a href=\"".concat(window.location.origin, "/movie/").concat(title, "\">").concat(title, "</a></li>"));
+        });
+    });
+};
+var moviePageHandler = function (movieTitle) {
+    var $displayArea = $("#movieInfo");
+    getDataThen(function (movies) {
+        var movie = movies.find(function (movie) { return movie.title === movieTitle; });
+        var actors = __spreadArray([], __read(new Set(movie.cast)), false);
+        var genres = __spreadArray([], __read(new Set(movie.genres)), false);
+        $displayArea.append("<li>Year Released: ".concat(movie.year, "</li>"));
+        if (genres.length > 0) {
+            $displayArea.append("<li>Movie Genres:</li>");
+            genres.forEach(function (genre) {
+                $displayArea.append("<li>".concat(genre, "</li>"));
+            });
+        }
+        if (actors.length > 0) {
+            $displayArea.append("<li>Movie Cast (".concat(actors.length, " actors):</li>"));
+            actors.forEach(function (actor) {
+                $displayArea.append("<li><a href=\"".concat(window.location.origin, "/actor/").concat(actor, "\">").concat(actor, "</a></li>"));
+            });
+        }
+    });
 };
